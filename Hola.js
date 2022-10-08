de514ffd94451685010547ec7f9564678e371150
RaulP18 <112339123+RaulP18@users.noreply.github.com>
2022-10-08 18:35:41 -0500

    modificaciones al reto1

--- Hola.js
+++ Hola.js
@@ -1,1 +1,2 @@
-console.log("Hola soy el archivo Hola.js")
+console.log("Hola soy el archivo Hola.js");
+console.log("Soy una modificacion al archivo original.js");
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # repositorio-git-reto1
+console.log("Hola soy el archivo Hola.js")
